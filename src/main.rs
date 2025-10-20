262f79348bf185e9569fe71c8614cf7a929259f8
kanta1207 <kan0712livepool@gmail.com>
2025-10-20 16:24:41 -0700

    print poem

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 use std::env;
+use std::fs::File;
+use std::io::prelude::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -7,5 +9,14 @@ fn main() {
     let file_path = &args[2];
 
     println!("Searching for {} in {}", query, file_path);
+
+    println!("In file {}", file_path);
+
+    let mut f = File::open(file_path).expect("ファイルが開けませんでした");
+
+    let mut contents = String::new();
+    f.read_to_string(&mut contents).expect("ファイルが読み込めませんでした");
+
+    println!("File contents: {}", contents);
 }
 
