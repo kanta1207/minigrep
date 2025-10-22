f2250854f091ad70a3583244f9c5885a38340e10
kanta1207 <kan0712livepool@gmail.com>
2025-10-21 23:21:00 -0700

    refactor error handling using Result

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
+    let config = generate_config(&args);
 
     println!("Searching for {} in {}", config.query, config.filename);
 
@@ -19,16 +20,26 @@ fn main() {
     println!("File contents: {}", contents);
 }
 
+fn generate_config(args: &[String]) -> Config {
+    Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    })
+}
+
 struct Config {
     query: String,
     filename: String,
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+         return Err("not enough arguments");
+        }
         let query = args[1].clone();
         let filename = args[2].clone();
 
-        Config { query, filename }
+        Ok(Config { query, filename })
     }
 }
