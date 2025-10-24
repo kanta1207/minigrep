395fa4558f9c024f3ccca3bae1b2c3af0b58a4e0
kanta1207 <kan0712livepool@gmail.com>
2025-10-24 01:40:44 -0700

    env

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,12 @@
 extern crate minigrep;
 
-use std::env;
 use std::process;
 
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
+    let args = minigrep::get_args();
 
-    let config = minigrep::generate_config(&args);
-    println!("Searching for {} in {}", config.query, config.filename);
-
-    println!("In file {}", config.filename);
+    let config = minigrep::generate_config(&args); 
 
     
     if let Err(e) = minigrep::run(config) {
